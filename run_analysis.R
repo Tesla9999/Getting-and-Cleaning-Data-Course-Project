a0f52b34a3de801042720eaf3c63286348f5c486
Tesla9999 <mdai78@gmail.com>
2016-05-28 09:49:09 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -21,56 +21,57 @@ y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
 subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
 
 features <- read.table("UCI HAR Dataset/features.txt")
-act_label <- read.table("UCI HAR Dataset/activity_labels.txt")
+activity_label <- read.table("UCI HAR Dataset/activity_labels.txt")
 
 # 1. Merges the training and the test sets to create one data set
-## consolidate data by rows
 
-test_val <- rbind(x_test, x_train) # 10299 (row) x 561 (col) 
-test_label <- rbind(y_test, y_train) # 10299 (row) x 1 (col)
-subject <- rbind(subject_test, subject_train) # 10299 (row) x 1 (col)
+merged_data <- rbind(x_test, x_train) # 10299 (row) x 561 (col) 
 
-## assign names for each variable
-
-names(subject) <- "subject"
-names(test_val) <- features$V2
-names(test_label) <- "activity"
-
-## fianl merged dataset by column
-
-merged_data <- cbind(subject, test_val, test_label) # 10299 (row) x 563 (col)
 
 # 2. Extracts the measurements on mean and standard deviation 
+
 ## indexing if mean or std in the name
 
-index<-grep("mean\\(\\)|std\\(\\)", features$V2)
+index <- grep("mean()|std()", features$V2)
 
-## subset by column, use names(extracted) to check
+## extract the dataset on mean and standard deviation
 
-extracted <- test_val[,index] # 10299 (row) x 563 (col)
+extract <- merged_data[,index] # 10299 (row) x 79 (col)
 
-# 3. Uses descriptive activity names to name the activities in the data set
-## change the numeric "acitivity" column in merged data to factors
+# 3. Uses descriptive activity names to name the activities in the dataset
+# 4. Appropriately labels the data set with descriptive variable names
 
-merged_data$activity <- as.factor(merged_data$activity)
+## combine the subjects and acitivity data
 
-## rename the numbered factors to char factors
+activity <- rbind(y_test, y_train) # 10299 (row) x 1 (col)
+subject <- rbind(subject_test, subject_train) # 10299 (row) x 1 (col)
 
-levels(merged_data$activity) <- act_label$V2
+## give descriptive names to each dataset
 
-# 4. Uses descriptive activity names to name the activities in the data set
-## it has already beed done previously by names(test_val) <- features$V2
-## before merging all the dataset (line #36), check using names(merged_data)
+names(subject) <- "subject"
+names(activity) <- "activity"
+names(extract) <- features$V2[index] # only names for mean and std
+
+## fianl extracted dataset with descriptive names for every column
+
+extracted_data <- cbind(subject, activity, extract) # 10299 (row) x 81 (col)
+
+## rename activity using descriptive activity names in the dataset
+
+activity_names <- factor(extracted_data$activity)
+levels(activity_names) <- activity_label$V2
+extracted_data$activity <- activity_names
 
 # 5. creates an independent tidy data set with the average of each variable 
-## change the "subject" column to factor
 
-merged_data$subject <- as.factor(merged_data$subject)
+## convert subject column to factor
+
+extracted_data$subject <- as.factor(extracted_data$subject)
 
 ## calculate mean of all rows, grouped by the 2 factors, "subject" and "activity"
-## row 2-562 are selected to exclude "subject" and "activity" (row 1 and 563)
+## "subject" and "activity" (row 1 and 2) are excluded from summerizing
 
-tidydata <- aggregate(merged_data[, 2:562], by =list(subject = 
-                      merged_data$subject, activity = merged_data$activity),
+tidydata <- aggregate(extracted_data[, -c(1:2)], by =list(subject = 
+                      extracted_data$subject, activity = extracted_data$activity),
                       mean, na.rm =TRUE)
 write.table(tidydata, "tidydata.txt", row.name=FALSE)
